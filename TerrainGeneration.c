13ad2b6db99b83f06cd58f9948abd53caeb4224e
Akpobari Godpower <godpower@iastate.edu>
2022-01-29 19:15:42 -0600

    Path is finished

--- TerrainGeneration.c
+++ TerrainGeneration.c
@@ -20,91 +20,71 @@ int main(int argc, char *argv[]){
         }
     }
     //possible bug if one of the random values are within
-    int N = rand()% 80 + 1;
-    int S = rand()% 80 + 1;
+    int N = rand()% 78 + 1;
+    int S = rand()% 78 + 1;
     int E = rand()% 19 + 1;
     int W = rand()% 19 + 1;
-    int ranX = rand()% 80 + 1;
-    int ranY = rand()% 21 + 1;
-    int x_diff,x_t_diff= E - W;
-    int y_diff,y_t_diff = N - S;
-    printf("N:%d, S:%d, diffpos: %d, diffneg: %d\n",N,S,x_diff,x_t_diff);
+    //Make sure the bounds of ranX and Y are properly defined so that it does not go of the map 
+    int ranX = rand()% 78 + 1;
+    int ranY = rand()% 19 + 1;
+    int x_diff = E - W;
+    int x_t_diff = E - W;
+    int y_diff= N - S;
+    int y_t_diff = N - S;
+    printf("X difference: %d, Y difference: %d\n",x_diff,y_diff);
+    printf("N:%d, S:%d\n",N,S);
     printf("E:%d, W:%d, ranY: %d\n",E,W,ranY);
     map[0][N] = '#';
     map[20][S] = '#';
     map[E][0] = '#';
     map[W][79] = '#';
-    
+    int k = 79;
 
     //Makes path along East to West
-    /*
+    
     for(i = 0; i < 21; i++){
         for(j = 0; j < 80; j++){
             if(i == E && j < ranX && j > 0){
-                map[E][j] = 'P';
+                map[E][j] = '#';
             }
-        }
-    }
-
-    for(i = 0; i < 21; i++){
-        for(j = 0; j < 80; j++){ 
             if(E > W &&  x_diff>= 0){
-                map[E-x_diff][ranX - 1] = '*';
+                map[E-x_diff][ranX] = '#';
                 x_diff--;
             }else if(E < W && x_diff <= 0){
-                map[E-x_diff][ranX - 1] = '*';
+                map[E-x_diff][ranX] = '#';
                 x_diff++;
             }
+            if(k > ranX)
+            {
+                map[W][k] = '#';
+                k--;
+            }
         }
     }
 
-  
-    for(i = 0; i < 21; i++){
-        for(j = 0; j < 80; j++){
-            if(j < 79 && ranX < 79)
-            {
-                map[E-x_t_diff][ranX] = '*';
-                ranX++;
-            }
-            
-        }
-    }*/
     //makes path along West to East
-    for(i = 0; i < 20; i++){
-        for(j = 0; j < 80; j++){
-            if(j == N && i < ranY && i > 0){
-                 map[i][N] = 'P';
-            }
+int f = 20;
+for(i = 0; i < 21; i++){
+    for(j = 0; j < 80; j++){
+        if(j == N && i <= ranY && i > 0){
+            map[i][N] = '#';
         }
-    }
-//Stuck on connecting with north and south axis
-  //  map[N+y_diff][ranY] = '*';
-/*
-    for(i = 0; i < 21; i++){
-        for(j = 0; j < 80; j++){ 
-            if(N > S &&  y_diff>= 0){
-                map[N-y_diff][ranY - 1] = '*';
-                y_diff--;
-            }else if(N < S && y_diff <= 0){
-                map[N-y_diff][ranY - 1] = '*';
-                y_diff++;
-            }
-        }
-    }
 
-  /*
-    for(i = 0; i < 21; i++){
-        for(j = 0; j < 80; j++){
-            if(j < 79 && ranX < 79)
-            {
-                map[E-x_t_diff][ranX] = '*';
-                ranX++;
-            }
-            
-        }
-    }
+        if(N > S && y_diff > 0){
+               map[ranY][N-y_diff] = '#';
+               y_diff--;
 
-*/
+           }else if(N < S && y_diff < 0){
+               map[ranY][N-y_diff] = '#';
+               y_diff++;
+           }
+
+        if(f > ranY)
+             map[f][S] = '#';
+             f--;
+    }
+}
+
 
 
     for(i = 0; i < 21; i++){
